ba8d7b077df7a9ff3a3d9c8562b2f5df68c1c885
Christian Gram Kalhauge <christian@kalhauge.dk>
2020-10-01 18:54:53 +0200

    I did it?

--- W40/issorted.py
+++ W40/issorted.py
@@ -6,6 +6,12 @@ def issorted(lst):
     
     >>> issorted([3])
     True
+    
+    >>> issorted([3, 2])
+    False
+    
+    >>> issorted([2, 3])
+    True
 
     >>> issorted([7, 9, 13])
     True
@@ -14,13 +20,24 @@ def issorted(lst):
     False
 
     """
-
-    return False
+    
+    if len(lst) <= 1:
+        return True
+    
+    last_seen, *rest = lst
+    for i in rest:
+        if last_seen <= i:
+            last_seen = i
+        else:
+            return False
+    
+    return True
 
 
 # this helps us test the code, is only run if we
 # run the module:
 if __name__ == '__main__':
     import doctest
-    doctest.testmod()
+    if doctest.testmod().failed == 0:
+        print("Looks good man!")
 
